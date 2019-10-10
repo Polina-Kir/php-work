4cdf5b6c5483d631935df7c1d666c3dc0efeffb3
Polina-Kira <polina.hohlatsova@ttu.ee>
2019-10-10 12:43:19 +0300

    3

--- index.php
+++ index.php
@@ -9,63 +9,22 @@ $request = new Request($_REQUEST);
 
 $cmd = $request->param('cmd')
     ? $request->param('cmd')
-    : 'list_form';
+    : 'list_page';
 
-if ($cmd === 'list_form') {
+if ($cmd === 'list_page') {
     $data = [
         'title' => 'Nimekiri',
         'template' => 'list.html',
-
     ];
-
     print renderTemplate('list.html', $data);
 
-} else if ($cmd === 'add_form') {
-
+} else if ($cmd === 'add_page') {
     $data = [
         'title' => 'Lisa',
         'template' => 'add.html',
-
     ];
-
     print renderTemplate('add.html', $data);
 
-} /*else if ($cmd === 'ctf_calculate') {
-
-    $input = intval($request->param('temperature'));
-    $result = celsiusToFahrenheit($input);
-    $message = "$input degrees in Celsius is $result degrees in Fahrenheit";
-
-    $data = [
-
-        'template' => 'ex4_result.html',
-        'message' => $message
-    ];
-
-    print renderTemplate('templates/ex4_main.html', $data);
-
-} else if ($cmd === 'ftc_calculate') {
-
-    $input = intval($request->param('temperature'));
-    $result = fahrenheitToCelsius($input);
-    $message = "$input degrees in Fahrenheit is $result degrees in Celsius";
-    $data = [
-
-        'template' => 'ex4_result.html',
-        'message' => $message
-    ];
-
-    print renderTemplate('templates/ex4_main.html', $data);
-
-} */else {
+} else {
     throw new Error('programming error');
 }
-
-/*function fahrenheitToCelsius($temp) {
-    return round(($temp - 32) / (9 / 5), 2);
-}
-
-function celsiusToFahrenheit($temp) {
-    return round($temp * 9 / 5 + 32, 2);
-}*/
-
