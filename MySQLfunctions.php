bd147e8bd12491aebe184fa97a429092d9668396
Polina-Kira <polina.hohlatsova@ttu.ee>
2019-11-02 16:22:25 +0200

    5

--- MySQLfunctions.php
+++ MySQLfunctions.php
@@ -4,8 +4,7 @@ const USERNAME = "khokhlacheva";
 const PASSWORD = "7902";
 const URL = "mysql:host=db.mkalmo.xyz;dbname=khokhlacheva";
 
-function addPerson($Contact)
-{
+function addPerson($Contact){
     $connection = new PDO(URL, USERNAME, PASSWORD);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
@@ -14,14 +13,11 @@ function addPerson($Contact)
     $statement->bindValue(":first", $Contact->firstName);
     $statement->bindValue(":last", $Contact->lastName);
     $statement->execute();
-
     $contactId = $connection->lastInsertId();
-
     $statement = $connection->prepare("insert into khokhlacheva.phones (contact_id, phone) 
 values(:id, :phone)");
     $statement->bindValue(":id", $contactId);
     $statement->bindValue(":phone", $Contact->phone1);
-
     $statement->execute();
 
     if ($Contact->phone2 != "") {
@@ -29,7 +25,6 @@ values(:id, :phone)");
 values(:id, :phone)");
         $statement->bindValue(":id", $contactId);
         $statement->bindValue(":phone", $Contact->phone2);
-
         $statement->execute();
     }
 
@@ -38,39 +33,33 @@ values(:id, :phone)");
 values(:id, :phone)");
         $statement->bindValue(":id", $contactId);
         $statement->bindValue(":phone", $Contact->phone3);
-
         $statement->execute();
     }
-
-
 }
 
-function getPersons()
-{
+function getPersons(){
     $connection = new PDO(URL, USERNAME, PASSWORD);
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     $statement = $connection->prepare("select * from contacts, phones
     where contacts.id = phones.contact_id;");
     $statement->execute();
 
-    $Persons = [];
+    $persons = [];
     foreach ($statement as $row) {
         $id = $row["contact_id"];
         $firstName = $row["firstName"];
         $lastName = $row["lastName"];
-        if (array_key_exists($id, $Persons)) {
-            if ($Persons[$id]->phone2 == "") {
-                $Persons[$id]->phone2 = $row["phone"];
-            } elseif ($Persons[$id]->phone3 == "") {
-                $Persons[$id]->phone3 = $row["phone"];
+        if (array_key_exists($id, $persons)) {
+            if ($persons[$id]->phone2 == "") {
+                $persons[$id]->phone2 = $row["phone"];
+            } elseif ($persons[$id]->phone3 == "") {
+                $persons[$id]->phone3 = $row["phone"];
             }
         } else {
             $phone = $row["phone"];
-            $Person = new item($firstName, $lastName, $phone);
-            $Persons[$id] = $Person;
+            $person = new item($firstName, $lastName, $phone);
+            $persons[$id] = $person;
         }
-
     }
-    return array_values($Persons);
+    return array_values($persons);
 }
--- item.php
+++ item.php
@@ -1,15 +1,13 @@
 <?php
 
-class item
-{
+class item{
     public $firstName;
     public $lastName;
     public $phone1;
     public $phone2;
     public $phone3;
 
-    public function __construct($firstName, $lastName, $phone1, $phone2 = "", $phone3 = "")
-    {
+    public function __construct($firstName, $lastName, $phone1, $phone2 = "", $phone3 = ""){
         $this->firstName = $firstName;
         $this->lastName = $lastName;
         $this->phone1 = $phone1;
@@ -17,8 +15,7 @@ class item
         $this->phone3 = $phone3;
     }
 
-    public function __toString()
-    {
+    public function __toString(){
         return "item{firstName:$this->firstName, lastName:$this->lastName, phone:$this->phone1}";
     }
 }
